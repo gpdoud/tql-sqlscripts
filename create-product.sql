cc6fce3fcadf32d83c3690e4d134cdc54d3c9602
Greg Doud <gpdoud@gmail.com>
2021-05-17 19:47:37 -0400

    Updated create-product.sql

---
+++ create-product.sql
@@ -0,0 +1,12 @@
+create or alter AddProduct
+    @partnbr varchar(30) = null,
+    @description varchar(30) = null,
+    @price decimal(11,2) = null,
+    @unit varchar(10) = null,
+    @photopath varchar(255) = null,
+    @vendorname varchar(30) = null
+as
+begin
+
+end
+go
